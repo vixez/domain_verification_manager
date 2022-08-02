58bf0802b088218cb12b54efd17e4460407083db
Glenn Ruysschaert <glennruysschaert@gmail.com>
2022-08-02 09:45:30 +0200

    ex: update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:after_layout/after_layout.dart';
 import 'package:domain_verification_manager/domain_verification_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -15,36 +16,40 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> with AfterLayoutMixin<MyApp> {
   String _isSupported = 'Unknown';
   String _domainStateVerified = 'Unknown';
   String _domainStateSelected = 'Unknown';
   String _domainStateNone = 'Unknown';
 
   @override
-  void initState() {
-    super.initState();
-    getIsSupported();
-    getDomainStageVerified();
-    getDomainStateSelected();
-    getDomainStateNone();
+  FutureOr<void> afterFirstLayout(BuildContext context) async {
+    if (await getIsSupported()) {
+      getDomainStageVerified();
+      getDomainStateSelected();
+      getDomainStateNone();
+    }
   }
 
-  Future<void> getIsSupported() async {
+  Future<bool> getIsSupported() async {
     String result;
+    bool _supported = false;
     try {
-      result = (await DomainVerificationManager.isSupported).toString();
+      _supported = await DomainVerificationManager.isSupported;
+      result = _supported.toString();
     } on PlatformException {
       result = 'Failed to get getIsSupported';
     }
     if (!mounted) {
       _isSupported = result;
-      return;
+      return _supported;
     }
 
     setState(() {
       _isSupported = result;
     });
+
+    return _supported;
   }
 
   Future<void> getDomainStageVerified() async {
